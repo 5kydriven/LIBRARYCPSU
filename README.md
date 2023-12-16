77c68fcc7daa44965a2bab300920ac841696c434
yoyan1 <yoyan947@gmail.com>
2023-12-16 09:45:14 +0800

    testing

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # LIBRARYCPSU
+# Testing 
--- index.php
+++ index.php
@@ -1,6 +1,4 @@
 <?php
-
-
 date_default_timezone_set('Asia/Manila');
 	 $time = date("h:i A",strtotime("-8 HOURS"));
    $date = date("M-d-Y l",strtotime("-8 HOURS"));
